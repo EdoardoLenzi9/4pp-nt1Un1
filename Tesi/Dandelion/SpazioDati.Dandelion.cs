7aa621437fc1e2980ab2e8e2ba8555210121b190
dodo <edoardolenzi9@gmail.com>
2017-10-25 21:20:51 +0200

    lfc e sacchi dispense

---
+++ Tesi/Dandelion/SpazioDati.Dandelion.cs
@@ -0,0 +1,18 @@
+Se mi cambiate gli end point?
+
+
+Entity extraction
+Text similarity
+Text calssification
+Language Detection 
+Sentiment Analysis
+WikiSearch
+
+https://api.dandelion.eu/datatxt/nex/v1
+https://api.dandelion.eu/datatxt/sim/v1
+https://api.dandelion.eu/datatxt/cl/v1
+https://api.dandelion.eu/datatxt/li/v1
+https://api.dandelion.eu/datatxt/sent/v1
+https://api.dandelion.eu/datagraph/wikisearch/v1
+
+Tutte Get o Post
